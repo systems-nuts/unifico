548c2177a9d242bcf248c1a0fa81e608a7d8a802
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2020-10-19 21:33:46 +0100

    Done first boxplot script

---
+++ mapping/experiments/main.c
@@ -0,0 +1,47 @@
+#include <inttypes.h>
+
+uint64_t inc(uint64_t i) {
+    __asm__ __volatile__(
+        ""
+        : "+m" (i)
+        :
+        : "rax",
+          "rbx",
+          "rcx",
+          "rdx",
+          "rsi",
+          "rdi",
+          "rbp",
+          "rsp",
+          "r8",
+          "r9",
+          "r10",
+          "r11",
+          "r12",
+          "r13",
+          "r14",
+          "r15",
+          "ymm0",
+          "ymm1",
+          "ymm2",
+          "ymm3",
+          "ymm4",
+          "ymm5",
+          "ymm6",
+          "ymm7",
+          "ymm8",
+          "ymm9",
+          "ymm10",
+          "ymm11",
+          "ymm12",
+          "ymm13",
+          "ymm14",
+          "ymm15"
+    );
+    return i + 1;
+}
+
+int main(int argc, char **argv) {
+    (void)argv;
+    return inc(argc);
+}
--- spec2017/plot_overhead.py
+++ spec2017/plot_overhead.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+
 
 def extract_scores(csv_path, benchmark_num, iterations, start_line):
     """
@@ -14,22 +16,10 @@ def extract_scores(csv_path, benchmark_num, iterations, start_line):
     :return:
     """
     df = pd.read_csv(csv_path, skiprows=start_line - 1, nrows=benchmark_num * iterations)
-    benchmark_names = df['Benchmark']
-    scores_series = df['Est. Base Run Time']
-    scores = list(scores_series.apply(lambda x: float(x)))
-    aggregated_scores = [scores[x:x + iterations] for x in range(0, len(scores), iterations)]
 
-    aggregated_scores = np.array(aggregated_scores)
-    average_scores = [np.average(x) for x in aggregated_scores]
-    min_scores = [np.min(x) for x in aggregated_scores]
-    max_scores = [np.max(x) for x in aggregated_scores]
-    percentile_scores = [np.percentile(x, 95) for x in aggregated_scores]
-
-    print(benchmark_names)
-    print(average_scores)
-    print(min_scores)
-    print(max_scores)
-    print(percentile_scores)
+    sns.boxplot(x='Benchmark', y='Est. Base Run Time', hue='Category', data=df, palette='Set3')
+    plt.xticks(rotation=45)
+    plt.show()
 
 
 def plot_graph(x_Axis, y):
@@ -69,5 +59,4 @@ def plot_graph(x_Axis, y):
 
 if __name__ == '__main__':
 
-    extract_scores('results/test.csv', 10, 1, 7)
-    plot_graph([],[])
+    extract_scores('results/test.csv', 10, 4, 7)
