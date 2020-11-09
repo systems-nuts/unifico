c617fca84556552d9d8d071f48f5a7c863b92ac8
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2020-11-09 19:11:25 +0000

    Experiment: test

--- spec2017/run.sh
+++ spec2017/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmd1="python3.7 -m spec2017.run_spec --config-list spec2017/config/base.cfg --threads=1 --bench=600,602,605,625,657 --noreportable"
+cmd1="python3.7 -m spec2017.run_spec --config-list spec2017/config/clang_base.cfg --threads=1 --bench=600,602,605,625 --noreportable"
 cmd2="python3.7 -m spec2017.run_spec --config-list spec2017/config/base.cfg --full-thread-run --bench  600 602 605 625 657 --noreportable"
 
 if [ "$1" = "preview" ]; then
@@ -16,11 +16,12 @@ do
 
   export PATH=~/my_llvm/toolchain_exp/bin/:$PATH
   export LD_LIBRARY_PATH=$(llvm-config --libdir)
+  export SPEC_DIR=/home/nikos/cpu2017
   export SPEC_RESULT_DIR=${SPEC_SCRIPT_DIR}/results/${short_hash}
 
   git checkout "$short_hash"
 
-  cp "${SPEC_SCRIPT_DIR}"/config/base.cfg "${SPEC_DIR}"/config
+  cp "${SPEC_SCRIPT_DIR}"/config/clang_base.cfg "${SPEC_DIR}"/config/spec2017/config
   if [ ! -d "$SPEC_RESULT_DIR" ];then
     echo
     mkdir "$SPEC_RESULT_DIR"
@@ -28,7 +29,7 @@ do
 
   cp "${SPEC_SCRIPT_DIR}"/config/info.json "${SPEC_RESULT_DIR}"
 
-  bash -c "$cmd1"
+  sudo -E bash -c "$cmd1"
   #bash -c "$cmd2"
 
 done
