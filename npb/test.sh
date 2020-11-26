7d38250364ec6d34f2c22aa0ba67cc9cf8d843f0
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2020-11-26 19:17:20 +0000

    Bugfix: test.sh works with passed env vars

--- npb/test.sh
+++ npb/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cmd1="python3.7 -m npb.run_npb --suite-list test_suite.def --threads=1,2,4,6,8 --iterations=3"
-cmd2="python3.7 -m npb.run_npb --suite-list test_suite.def --threads=1,2,4,6,8,10,12,14,16 --iterations=3"
+cmd1="python3.7 -m npb.run_npb --suite-list suite.def --threads=1,2,4,6,8 --iterations=3"
+cmd2="python3.7 -m npb.run_npb --suite-list suite.def --threads=1,2,4,6,8,10,12,14,16 --iterations=3"
 
 if [ $1 = "preview" ]; then
 	cmd1+=" --preview"
@@ -14,7 +14,7 @@ echo "Testing..."
 echo "Give pass:"
 read -s PW
 
-export PATH=~/toolchain/bin/:$PATH
+export PATH=~/base_llvm/toolchain/bin/:$PATH
 export LD_LIBRARY_PATH=$(llvm-config --libdir)
 export NPB_RESULT_DIR=${NPB_SCRIPT_DIR}/results/test
 
@@ -27,8 +27,8 @@ mkdir "$NPB_RESULT_DIR"
 fi
 rm -f "$NPB_RESULT_DIR"/*
 
-echo $PW | sudo -S -E bash -c "$cmd1"
-echo $PW | sudo -S -E bash -c "$cmd2"
+echo $PW | sudo -S -E PATH=$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH bash -c "$cmd1"
+echo $PW | sudo -S -E PATH=$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH bash -c "$cmd2"
 
 for filename in "$NPB_RESULT_DIR"/*; do
 	[ -e "$filename" ] || exit
