9079eea05e4d02ed28d2bae461bf5cf6102f8b28
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-01-02 16:28:40 +0000

    Feature: Added program to check x86 clobber

---
+++ layout/clobber_x86.c
@@ -0,0 +1,19 @@
+#include <inttypes.h>
+
+uint64_t inc(uint64_t i) {
+	__asm__ __volatile__(
+			""
+			: "+m" (i)
+			:
+			: "r12",
+			  "r13",
+			  "r14",
+			  "r15"
+			);
+	return i + 1;
+}
+
+int main(int argc, char **argv) {
+	(void)argv;
+	return inc(argc);
+}
