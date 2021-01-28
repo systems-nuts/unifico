3e3ea4e723baca1742e8ad1aed7de901e7a6c20b
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-01-28 18:24:25 +0000

    Started iterating over llc flags

---
+++ layout/fact/fact.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// %rbx is used strangely here.
+// it gets from popq the result from the previous call to fact.
+int fact(int n)
+{
+	if (n < 1) {
+		int x = 0;
+		return 1;
+	}
+	else {
+		int x = 1;
+		return n * fact(n - 1);
+	}
+}
+
+int main()
+{
+	printf("fact(3)= %d\n", fact(3));
+	return 0;
+}
---
+++ layout/fact/llc_flag_assemblies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm *.s
+
+for regalloc in "basic" "fast" "greedy";do
+	llc_flags=-regalloc=$regalloc
+	for opt_flags in "" "-mem2reg";do
+		echo $llc_flags
+		echo $opt_flags
+		make clean
+		make OPT_FLAGS="$opt_flags" LLC_FLAGS="$llc_flags"
+		gdb -q fact.out -x ../gdb_fact
+		echo '================'
+	done
+done
