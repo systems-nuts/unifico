610adb8bbaedc2e2826086c278bfbe4841b75c88
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-03-04 13:49:03 +0000

    Added musl to Makefile

--- layout/test_align/call_leaf/check_sections.sh
+++ layout/test_align/call_leaf/check_sections.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-readelf --sections --wide call_leaf_x86-64 >x86-64_sections.txt
-readelf --sections --wide call_leaf_aarch64 >aarch64_sections.txt
-nm call_leaf_x86-64 >nm_x86-64.txt
-nm call_leaf_aarch64 >nm_aarch64.txt
+readelf --sections --wide is_x86-64 >x86-64_sections.txt
+readelf --sections --wide is_aarch64 >aarch64_sections.txt
+nm is_aarch64 >nm_aarch64.txt
+nm is_x86-64 >nm_x86-64.txt
 grep nm_aarch64.txt -e main
 grep nm_x86-64.txt -e main
 grep nm_aarch64.txt -e add_7
