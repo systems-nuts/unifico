6937789000a8598a946d6227d3c3b7db6e3ab397
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-04-14 18:52:50 +0100

    Stack Layout: minor adds

--- layout/call_leaf/examine.sh
+++ layout/call_leaf/examine.sh
@@ -4,4 +4,7 @@ make clean
 make check
 make asm
 gdb -q call_leaf_x86_64_aligned.out -x gdb_call_leaf
-#scp call_leaf_aarch64_aligned.out nikos@sole:~/phd/unified_abi/layout/call_leaf
+
+if [ $# -ne 0 ]; then
+	scp call_leaf_aarch64_aligned.out nikos@sole:~/phd/unified_abi/layout/call_leaf
+fi
---
+++ layout/masters_student.md
@@ -0,0 +1,6 @@
+## Tasks
+
+* Explain the difficulty to pinpoint cause and effect in LLVM Backend code.
+* Give a sufficient task up to the end of April.
+* Ask on the frame pointer.
+* Add him to the knowledge database.
