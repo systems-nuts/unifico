a43f95f4fa604152fac94b4616769f6b1674fd19
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-05-06 14:22:33 +0100

    Stack Layout: minor fixes

--- layout/fact/fact.c
+++ layout/fact/fact.c
@@ -5,17 +5,15 @@
 int fact(int n)
 {
 	if (n < 1) {
-		int x = 0;
 		return 1;
 	}
 	else {
-		int x = 1;
 		return n * fact(n - 1);
 	}
 }
 
 int main()
 {
-	printf("fact(4)= %d\n", fact(4));
+	printf("fact(5)= %d\n", fact(5));
 	return 0;
 }
--- layout/masters_student.md
+++ layout/masters_student.md
@@ -1,6 +1,5 @@
 ## Tasks
 
 * Explain the difficulty to pinpoint cause and effect in LLVM Backend code.
-* Give a sufficient task up to the end of April.
 * Ask on the frame pointer.
 * Add him to the knowledge database.
