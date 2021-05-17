5a1798b037755373626b6c05eb0cd824dbb37064
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-05-17 18:58:02 +0100

    Stack Layout: Added bubblesort example

--- layout/bubble_sort/bubble_sort.c
+++ layout/bubble_sort/bubble_sort.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 void swap(int v[], int k)
 {
@@ -5,4 +6,26 @@ void swap(int v[], int k)
 	temp = v[k];
 	v[k] = v[k + 1];
 	v[k + 1] = temp;
+}
+
+void sort(int v[], int n)
+{
+	int i, j;
+	for (i = 0; i < n; i += 1) {
+		for (j = i - 1; j >= 0 && v[j] > v[j + 1]; j -= 1) {
+			swap(v,j);
+		}
+	}
+}
+
+int main()
+{
+	int v[5] = { 4, 1, 3, 2, 1 };
+	
+	sort(v, 5);
+
+	for (int i = 0; i < 5; ++i)
+		printf("v[%d]=%d\n", i, v[i]);
+
+	return 0;
 }
