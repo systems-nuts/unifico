054c69f84cb55c75e3738acfdcb4f0a0015f7755
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-07-08 14:01:22 +0100

    [stack-layout] Added plotting for llvm-mca tool for hardware pressure analysis

--- hardware_pressure/llvm_mca_wrapper/extract_stats.py
+++ hardware_pressure/llvm_mca_wrapper/extract_stats.py
@@ -1,6 +1,9 @@
+import os
+import matplotlib.pyplot as plt
+
 from jsonstream import load
 
-EXAMPLE_JSON = 'json_examples/fact_x86-64.json'
+EXAMPLE_JSON = 'json_examples/fact_aarch64.json'
 
 
 def parse_mca_json(file_path):
@@ -55,6 +58,52 @@ def resource_pressure(mca_dict):
     }
 
 
+def sort_by_pressure(folder_path, target_resources):
+    """Sort the assembly files in a folder based on hardware pressure
+
+    Calculates the hardware pressure for the given resources, for all the resources of the JSON files in a folder.
+    Returns a list with the hardware pressure in ascending order.
+    Hardware pressure per file is summed for all the resources.
+    JSON files are created through the `llvm-mca --json` command and must be of the same architecture.
+    @param folder_path: the location of the json files
+    @param target_resources: list of processor resources
+    @return: list with program names with their pressures in ascending order
+    """
+    result = []
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        mca_dict = parse_mca_json(file_path)
+        pressure_dict = resource_pressure(mca_dict)
+        total_pressure = sum([pressure_dict[resource]
+                              for resource in pressure_dict.keys()
+                              if resource in target_resources])
+        result.append([file, total_pressure])
+
+    result = sorted(result, key=lambda x: x[1])  # Sort list of tuples based on the 2nd argument, i.e. total_pressure
+
+    return list(zip(*result))
+
+
+def plot_by_pressure(folder_path, target_resources):
+    """Plot the assembly files in a folder based on hardware pressure
+
+    Calculates the hardware pressure for the given resources, for all the resources of the JSON files in a folder.
+    Plots the hardware pressure for every in ascending order.
+    Hardware pressure per file is summed for all the resources.
+    JSON files are created through the `llvm-mca --json` command and must be of the same architecture.
+    @param folder_path: the location of the json files
+    @param target_resources: list of processor resources
+    @return:
+    """
+    pressure_list = sort_by_pressure(folder_path, target_resources)
+    plt.bar(pressure_list[0], pressure_list[1])
+    plt.show()
+
+
 if __name__ == '__main__':
     mca = parse_mca_json(EXAMPLE_JSON)
     print(resource_pressure(mca))
+    print(sort_by_pressure('json_examples', ['JALU0', 'JDiv']))
+    print(sort_by_pressure('json_examples', ['THX2T99P0']))
+    plot_by_pressure('json_examples', ['JALU0', 'JDiv'])
+    plot_by_pressure('json_examples', ['THX2T99P0'])
