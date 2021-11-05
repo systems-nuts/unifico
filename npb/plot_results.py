4ddda5e704c659944f60baf1e6e452152d4e6fb8
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-11-05 18:57:32 +0000

    [layout][experiments] Added tests for NPB plotting functions.

--- npb/plot_results.py
+++ npb/plot_results.py
@@ -7,6 +7,18 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns
 
+# This verbose and more readable regex form, requires the re.VERBOSE flag in re.compile
+RESULT_FILE_REGEX = re.compile(r"""
+ (\w+)      # Benchmark 
+ \.         # Delimiter
+ (\w+)      # Architecture 
+ \.         # Delimiter
+ (\w+)      # NPB Class
+ \.         # Delimiter
+ (\d+)      # Iteration
+ \.out      # Suffix
+""", re.VERBOSE)
+
 
 def get_info(json_path):
     """
@@ -32,35 +44,34 @@ def verify_npb_output(out_path):
         return False
 
 
-# TODO: tests
 def df_from_dir(results_dir):
     """
     Extract DataFrame from multiple NPB results in a directory.
-    @param results_dir: Directory containing multiple csvs from SPEC runs
+    @param results_dir: Directory containing multiple output files from NPB runs
     @return:
     """
     info_dict_path = '{}/info.json'.format(results_dir)
     if not os.path.isfile(info_dict_path):
         print('info.json not found')
-        exit(1)
+        experiment = results_dir.split('/')[-1]
+        flag = ''
+    else:
+        with open(info_dict_path, 'r') as fp:
+            info_dict = json.load(fp)
+        experiment = info_dict['experiment']['name']
+        # flag = info_dict['compiler']['flags']
+        flag = '-O1'
 
-    with open(info_dict_path, 'r') as fp:
-        info_dict = json.load(fp)
-    experiment = info_dict['experiment']
-    flag = info_dict['flag']
-
-    df = pd.DataFrame()
+    rows = []
 
     for output in os.listdir(results_dir):
-        pattern = re.compile(r'([a-z][a-z])\.([A-Z])_out\.(\d+)\.(compact|scatter)\.(\d+)')
-        match = pattern.search(output)
+        match = RESULT_FILE_REGEX.search(output)
         if match is None:
             continue
         bench = match.group(1)
-        bench_class = match.group(2)
-        threads = match.group(3)
-        affinity = match.group(4)
-        iteration = match.group(5)
+        arch = match.group(2)
+        bench_class = match.group(3)
+        iteration = match.group(4)
 
         output_abs_path = os.path.join(results_dir, output)
 
@@ -76,26 +87,27 @@ def df_from_dir(results_dir):
                     time = match.group(1)
             if not verified:
                 print('Not verified: ', output)
-                exit(1)
+                time = float('nan')
 
         out_dict = {
-            'Experiment': experiment,
+            'Experiment': experiment,  # TODO: redefine the notion of experiment
             'Flag': flag,
             'Benchmark': bench,
+            'Architecture': arch,
             'Class': bench_class,
-            'Threads': int(threads),
-            'Affinity': affinity,
             'Iteration': int(iteration),
             'Time': float(time)
         }
 
-        df = df.append(out_dict, ignore_index=True)
+        rows.append(out_dict)
 
-    df['Threads'] = df['Threads'].astype(int)
+    df = pd.DataFrame(rows)
+    df.to_csv('npb/results/csv/' + experiment + '.csv', index=False)
 
     return df
 
 
+# TODO: tests
 def dataframe_to_catplot(df):
     """
     @param df: Pandas DataFrame
@@ -109,18 +121,22 @@ def dataframe_to_catplot(df):
         plt.show()
 
 
-def dataframe_to_boxplot(df, hue='Experiment'):
+def dataframe_to_boxplot(df, arch, hue='Experiment'):
     """
     @param df: Pandas DataFrame
     @param hue: Comparison variable for boxplot
     """
-    for bench in set(df['Benchmark']):
-        sns.boxplot(x='Threads', y='Time', hue=hue,
-                    data=df[(df['Benchmark'] == bench)],
-                    palette='Set3')
-        plt.title(bench)
-        plt.ylabel('Time (s)')
-        plt.show()
+    # for bench in set(df['Benchmark']):
+    df['positive'] = df['% Overhead'] > 0
+    ax = df.plot.bar(x='Benchmark', y='% Overhead',
+                     color=df.positive.map({True: 'r', False: 'g'}))
+    # data=df,
+    # palette='Set1')
+    # plt.title(bench)
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+    plt.ylabel('% Overhead')
+    plt.title('{} overhead from \"{}\"- NPB class B'.format(arch, df['Experiment'][0]))
+    plt.show()
 
 
 def two_dataframes_boxplot(df1, df2):
@@ -149,7 +165,7 @@ def two_dataframes_boxplot(df1, df2):
         plt.show()
 
 
-def get_overhead_df(dir1, dir2):
+def get_overhead_df(dir1, dir2, arch):
     """
     Plot the graphs of the two experiments in each directory
     @param dir1: Output files for first experiment
@@ -165,33 +181,99 @@ def get_overhead_df(dir1, dir2):
         print('{} result directory does not exist.'.format(dir2))
 
     df1 = df_from_dir(dir1)
+    df1 = df1[df1['Architecture'] == arch]
     df1 = df1[df1['Class'] == 'B']
-    df1 = df1[df1['Threads'] == 1]
+    # df1 = df1[df1['Threads'] == 1]
     df2 = df_from_dir(dir2)
+    df2 = df2[df2['Architecture'] == arch]
     df2 = df2[df2['Class'] == 'B']
-    df2 = df2[df2['Threads'] == 1]
+    # df2 = df2[df2['Threads'] == 1]
+
+    exp1 = df1.iloc[0, 0]
+    exp2 = df2.iloc[0, 0]
+
+    # flag = info1['compiler']['flags'] TODO: always include info.json
+    flag = df2.iloc[0, 1]
 
     df1 = df1.groupby(['Benchmark']).mean()
     df2 = df2.groupby(['Benchmark']).mean()
 
-    info1 = get_info(os.path.join(dir1, 'info.json'))
-    info2 = get_info(os.path.join(dir2, 'info.json'))
-
-    exp1 = info1['experiment']
-    exp2 = 'remove 15 registers'
-    df2['Experiment'] = exp2
-    flag = info1['flag']
     title = '"{}" vs "{}" {}'.format(exp2, exp1, flag)
 
     total_df = df1.append(df2, ignore_index=True)
     # total_df.sort_values(by=['Benchmark', 'Class', 'Threads', 'Iteration'], inplace=True)
-    df1.sort_values(by=['Benchmark', 'Threads', 'Iteration'], inplace=True)
-    df2.sort_values(by=['Benchmark', 'Threads', 'Iteration'], inplace=True)
+    df1.sort_values(by=['Benchmark', 'Iteration'], inplace=True)
+    df2.sort_values(by=['Benchmark', 'Iteration'], inplace=True)
     df = pd.DataFrame(df1)
     df['% Overhead'] = df['Time'].combine(df2['Time'], lambda x1, x2: (x2 / x1 - 1) * 100)
     df.drop(['Iteration', 'Time'], axis=1, inplace=True)
+    df['Benchmark'] = df.index
+    df['Experiment'] = exp2
+    df.reset_index(drop=True, inplace=True)
 
     return df
+
+
+def compare_single_thread_exp(dir1, dir2, out_plot):
+    """
+    Plot the graphs of the two experiments in each directory
+    @param dir1: Output files for first experiment
+    @param dir2: Output files for first experiment
+    @param out_plot: Output dir
+    @return:
+    """
+    if not os.path.isdir(dir1):
+        print('{} result directory does not exist.'.format(dir1))
+    elif not os.path.isdir(dir2):
+        print('{} result directory does not exist.'.format(dir2))
+
+    df1 = df_from_dir(dir1)
+    df1 = df1[df1['Class'] == 'A']
+    df2 = df_from_dir(dir2)
+    df2 = df2[df2['Class'] == 'A']
+
+    info1 = get_info(os.path.join(dir1, 'info.json'))
+    info2 = get_info(os.path.join(dir2, 'info.json'))
+
+    exp1 = info1['experiment']['name']
+    exp2 = info2['experiment']['name']
+    exp2 = 'remove 15 registers'
+    df2['Experiment'] = exp2
+    flag = info1['flag']
+
+    total_df = df1.append(df2, ignore_index=True)
+    # total_df.sort_values(by=['Benchmark', 'Class', 'Threads', 'Iteration'], inplace=True)
+    df1.sort_values(by=['Benchmark', 'Class', 'Iteration'], inplace=True)
+    df1 = df1.reset_index(drop=True)
+    df2.sort_values(by=['Benchmark', 'Class', 'Iteration'], inplace=True)
+    df2 = df2.reset_index(drop=True)
+    df = pd.DataFrame(df1)
+    df['% Overhead'] = df['Time'].combine(df2['Time'], lambda x1, x2: (x2 / x1 - 1) * 100)
+    for bench in set(df['Benchmark']):
+        plt.figure(figsize=(10, 5))
+        plt.subplot(1, 2, 1)
+        sns.set_context(rc={"font.size": 14, "axes.titlesize": 24, "axes.labelsize": 16})
+        # sns.boxplot(x='Threads', y='Time', hue='Experiment',
+        #             data=total_df[(total_df['Benchmark'] == bench)],
+        #             palette='Set3')
+        sns.stripplot(x='Threads', y='Time', hue='Experiment',
+                      data=total_df[(total_df['Benchmark'] == bench)],
+                      size=6, color=".3", linewidth=0,
+                      palette='Set1')
+        plt.legend(loc=1, prop={'size': 8})
+        plt.ylabel('Time (s)')
+        plt.legend(fontsize='medium', title_fontsize='20')
+        plt.subplot(1, 2, 2)
+        sns.set_context(rc={"font.size": 14, "axes.titlesize": 24, "axes.labelsize": 16})
+        sns.boxplot(x='Threads', y='% Overhead', hue='Class',
+                    data=df[(df['Benchmark'] == bench)],
+                    palette='Set3')
+        plt.legend([], [], frameon=False)
+        bench_out_plot = '{}_{}'.format(out_plot, bench)
+        title = '{} - "{}" vs "{}" {}'.format(bench, exp2, exp1, flag)
+        plt.suptitle(title)
+        plt.savefig(bench_out_plot, bbox_inches='tight')
+        plt.show()
 
 
 def compare_experiments(dir1, dir2, out_plot, hue='Experiment', how='side'):
@@ -248,8 +330,8 @@ def compare_experiments(dir1, dir2, out_plot, hue='Experiment', how='side'):
         plt.subplot(1, 2, 2)
         sns.set_context(rc={"font.size": 14, "axes.titlesize": 24, "axes.labelsize": 16})
         sns.boxplot(x='Threads', y='% Overhead', hue='Class',
-                        data=df[(df['Benchmark'] == bench)],
-                        palette='Set3')
+                    data=df[(df['Benchmark'] == bench)],
+                    palette='Set3')
         plt.legend([], [], frameon=False)
         bench_out_plot = '{}_{}'.format(out_plot, bench)
         title = '{} - "{}" vs "{}" {}'.format(bench, exp2, exp1, flag)
@@ -259,18 +341,8 @@ def compare_experiments(dir1, dir2, out_plot, hue='Experiment', how='side'):
 
 
 if __name__ == '__main__':
-    # compare_experiments('results/1496895', 'results/remove_14_regs/ca1f7d6',
-    #                     'reports/plots/sole_remove_15_O1_B_large_font', hue='Class', how='overhead')
-
-    df0 = get_overhead_df('results/b779a20', 'results/remove_14_regs/096c020')
-    df0 = df0.rename(columns={'% Overhead': 'Overhead -O0'})
-    df1 = get_overhead_df('results/1496895', 'results/remove_14_regs/ca1f7d6')
-    df1 = df1.rename(columns={'% Overhead': 'Overhead -O1'})
-    df2 = get_overhead_df('results/b920081', 'results/remove_14_regs/1d570f6')
-    df2 = df2.rename(columns={'% Overhead': 'Overhead -O2'})
-    df3 = get_overhead_df('results/fdb187b', 'results/remove_14_regs/9776cd6')
-    df3 = df3.rename(columns={'% Overhead': 'Overhead -O3'})
-
-    df = pd.concat([df0, df1, df2, df3], axis=1)
-    df.drop(['Threads'], axis=1, inplace=True)
-    df.to_csv('results/remove_14_regs/overheads.csv')
+    for short_hash in ['d97be7cf96dc']:
+        df_overhead = get_overhead_df('npb/results/c6780392', 'npb/results/' + short_hash, arch='aarch64')
+        dataframe_to_boxplot(df_overhead, arch='aarch64')
+        df_overhead = get_overhead_df('npb/results/c6780392', 'npb/results/' + short_hash, arch='x86_64')
+        dataframe_to_boxplot(df_overhead, 'x86_64')
--- npb/test/test_run_npb.py
+++ npb/test/test_run_npb.py
@@ -1,6 +1,6 @@
 import pytest
 
-from unified_abi.npb.run_npb import parse_suite
+from npb.run_npb import parse_suite
 
 
 def test_parse_suite():
@@ -8,7 +8,7 @@ def test_parse_suite():
     Test whether parse suite returns the correct list of strings:
         'benchmark_name class'
     """
-    tuple_list = parse_suite('suite.def')
+    tuple_list = parse_suite('npb/test/suite.def')
     target_list = ['bt C', 'sp C', 'lu C', 'ft C', 'cg C', 'mg C', 'ep C', 'ua C', 'is C', 'dc B']
 
     assert set(tuple_list) == set(target_list), 'NPB parse suite error'
