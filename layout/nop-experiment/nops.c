3bad306c0d713024e40f448ac3f62835d2839f9b
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-11-15 18:49:24 +0000

    [layout] Improved printing for nop experiments.

--- layout/nop-experiment/nops.c
+++ layout/nop-experiment/nops.c
@@ -72,57 +72,57 @@ int main()
 	double t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop;
-	printf("nop time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop2;
-	printf("nop2 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop2 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop4;
-	printf("nop4 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop4 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop8;
-	printf("nop8 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop8 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop16;
-	printf("nop16 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop16 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop32;
-	printf("nop32 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop32 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop64;
-	printf("nop64 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop64 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop128;
-	printf("nop128 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop128 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop256;
-	printf("nop256 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop256 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop512;
-	printf("nop512 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop512 time: %.6lf us\n", what_time_is_it() - t0);
 
 	t0 = what_time_is_it();
 	for (int i = 0; i < ITERATIONS; ++i)
 		nop1024;
-	printf("nop1024 time: %.6lf\n", what_time_is_it() - t0);
+	printf("nop1024 time: %.6lf us\n", what_time_is_it() - t0);
 
 	return 0;
 }
