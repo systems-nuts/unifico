96ebadff4f736bee92f52383c99389b24f0fc13a
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-01-17 12:49:50 +0000

    Added simple script to print the overhead between two different NPB experiments.
    Addresses #11

---
+++ npb/compare_output.py
@@ -0,0 +1,43 @@
+import argparse
+
+from npb.plot_results import get_overhead_df
+
+if __name__ == '__main__':
+
+    arg_parser = argparse.ArgumentParser(
+        description=
+        'Compare NPB results for two different LLVM versions, with one '
+        'being the baseline.')
+
+    arg_parser.add_argument('baseline_dir',
+                            type=str,
+                            help='path for baseline result directory')
+
+    arg_parser.add_argument('test_dir',
+                            type=str,
+                            help='path for test result directory')
+
+    arg_parser.add_argument('-a',
+                            '--archs',
+                            required=False,
+                            help='delimited list of architectures',
+                            type=str,
+                            default='aarch64,x86_64')
+
+    arg_parser.add_argument('-c',
+                            '--npb_class',
+                            required=False,
+                            help='NPB class to examine',
+                            type=str,
+                            default='B')
+
+    args = arg_parser.parse_args()
+
+    for arch in args.archs.split(','):
+        df_overhead = get_overhead_df(args.baseline_dir, args.test_dir, arch,
+                                      args.npb_class)
+        print('Experiment: {}'.format(df_overhead['Experiment'][0]))
+        df_overhead.drop(['Experiment'], axis=1, inplace=True)
+        print('NPB overheads for architecture {}'.format(arch))
+        print(df_overhead)
+        print('----------------')
