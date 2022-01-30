70cb51dcf02955282ae8c29b677ee17342be7c91
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-01-30 20:45:11 +0000

    Setup failing test case with global const as a parameter.

--- layout/global-const/global.c
+++ layout/global-const/global.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-#define A 1220703125.0
+#define A 1.1
 
 double mul(double x, double y) {
 	return x * y;
 }
 
-double div(double x, double y) {
-	return x / y;
-}
-
-#define A 1220703125.0
 int main()
 {
 	double c1, c2, t;
 
 	t = A;
 	c1 = mul(t, t);
-	c2 = div(t, t);
-	printf("%f/n", c1 + c2);
+	c2 = c1;
+	printf("%f\n", c1 + c2);
 	return 0;
 }
