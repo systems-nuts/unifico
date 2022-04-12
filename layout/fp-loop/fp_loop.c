533d75f48fef83b0f1213782bf1b345d07d4ea10
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-04-12 13:47:31 +0100

    Simplified example of two calls from inside a loop with passing by reference.

--- layout/fp-loop/fp_loop.c
+++ layout/fp-loop/fp_loop.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 double fmul(double* x, double* y) {
-	static double r = 0.5;
-	r += 0.5;
-	return (*x) * (*y) * r;
+    static double r = 0.5;
+    r += 0.5;
+    return (*x) * (*y) * r;
 }
 
-// a loop with funciton call inside it
-double loop(double x, double y) {
-	double sum = 0;
-	for (int i = 0; i < 16; i++) {
-		sum = fmul(&x, &y);
-		sum += fmul(&x, &y);
-		sum += fmul(&x, &y);
-		sum += fmul(&x, &y);
-	}
-	return sum;
-}
 
 int main()
 {
-	double res = loop(3.5, 4.5);
-	printf("%f", res);
-	return 0;
+    double x = 3.5;
+    double y = 4.5;
+    double sum = 0;
+    for (int i = 0; i < 16; i++) {
+        sum = fmul(&x, &y);
+        sum += fmul(&x, &y);
+    }
+    printf("%f", sum);
+    return 0;
 }
