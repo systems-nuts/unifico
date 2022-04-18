52247127bc08add2f5b902d44468c2c3f94e273f
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-04-18 13:17:42 +0100

    Added global-array example.

---
+++ layout/global-array/global.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int global_array[30];
+
+
+int main()
+{
+    int x = 3;
+	global_array[x] = 5;
+	for (int i = 0; i < 10; i++) {
+        printf("%d\n", global_array[x * i]);
+	}
+	return 0;
+}
