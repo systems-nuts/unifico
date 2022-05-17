c9626b3408d03ec2aa7e40d13262102838a4ca21
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-05-17 10:51:32 +0100

    Fix imports and paths for unit tests

--- layout/callsites/test/test_callsite_align.py
+++ layout/callsites/test/test_callsite_align.py
@@ -1,7 +1,9 @@
 import pytest
 
-from layout.callsites.callsite_align import *
-from layout.callsites.check_callsite_align import check_callsite_align
+from align.callsite_align import align, get_return_addresses, check_callsite_number
+from align.compare import compare_callsite_align
+
+TEST_INPUT_PATH = 'test/input/'
 
 # TODO: add test for get_return_addresses
 
@@ -11,8 +13,8 @@ def test_align():
     Test whether the function properly aligns the callsites given two objdump files,
     by returning the correct padding dictionary.
     """
-    input1 = 'layout/callsites/test/pass/objdump-arm.txt'
-    input2 = 'layout/callsites/test/pass/objdump-x86.txt'
+    input1 = TEST_INPUT_PATH + 'pass/objdump-arm.txt'
+    input2 = TEST_INPUT_PATH + 'pass/objdump-x86.txt'
 
     padding_dict = align(input1, input2)
 
@@ -34,8 +36,8 @@ def test_different_callsite_number_pass():
     """
     Test whether the function properly detects different number of callsites in the objdump files.
     """
-    input1 = get_return_addresses('layout/callsites/test/pass/objdump-arm.txt', 'aarch64')
-    input2 = get_return_addresses('layout/callsites/test/pass/objdump-x86.txt', 'x86-64')
+    input1 = get_return_addresses(TEST_INPUT_PATH + 'pass/objdump-arm.txt', 'aarch64')
+    input2 = get_return_addresses(TEST_INPUT_PATH + 'pass/objdump-x86.txt', 'x86-64')
 
     check_callsite_number(input1, input2)
 
@@ -44,8 +46,8 @@ def test_different_callsite_number_fail():
     """
     Test whether the function properly detects different number of callsites in the objdump files.
     """
-    input1 = get_return_addresses('layout/callsites/test/fail/objdump-arm.txt', 'aarch64')
-    input2 = get_return_addresses('layout/callsites/test/fail/objdump-x86.txt', 'x86-64')
+    input1 = get_return_addresses(TEST_INPUT_PATH + 'fail/objdump-arm.txt', 'aarch64')
+    input2 = get_return_addresses(TEST_INPUT_PATH + 'fail/objdump-x86.txt', 'x86-64')
 
     print()
     with pytest.raises(SystemExit) as e:
@@ -54,25 +56,25 @@ def test_different_callsite_number_fail():
     assert e.value.code == 1
 
 
-def test_check_callsite_align_pass():
+def test_compare_callsite_align_pass():
     """
     Test whether the function properly checks that the callsites of the two objdump files are properly aligned.
     """
-    input1 = 'layout/callsites/test/pass/aligned-objdump-arm.txt'
-    input2 = 'layout/callsites/test/pass/aligned-objdump-x86.txt'
+    input1 = TEST_INPUT_PATH + 'pass/aligned-objdump-arm.txt'
+    input2 = TEST_INPUT_PATH + 'pass/aligned-objdump-x86.txt'
 
-    check_callsite_align(input1, input2)
+    compare_callsite_align(input1, input2)
 
 
-def test_check_callsite_align_fails():
+def test_compare_callsite_align_fails():
     """
     Test whether the function properly checks that the callsites of the two objdump files are not properly aligned.
     """
-    input1 = 'layout/callsites/test/pass/objdump-arm.txt'
-    input2 = 'layout/callsites/test/pass/objdump-x86.txt'
+    input1 = TEST_INPUT_PATH + 'pass/objdump-arm.txt'
+    input2 = TEST_INPUT_PATH + 'pass/objdump-x86.txt'
 
     print()
     with pytest.raises(SystemExit) as e:
-        check_callsite_align(input1, input2)
+        compare_callsite_align(input1, input2)
     assert e.type == SystemExit
     assert e.value.code == 1
