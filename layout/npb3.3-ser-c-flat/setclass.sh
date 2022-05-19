a98fd954f8d17e1e5caa0c1833496e6ece660fab
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-05-19 12:42:02 +0100

    Clean up and add class restrictions

--- layout/npb3.3-ser-c-flat/setclass.sh
+++ layout/npb3.3-ser-c-flat/setclass.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 {S | A | B}"
-	exit 1
-fi
+HELP_STR="Usage: $0 {S | A | B | C}"
+HEADER_FILE=npbparams.h
 
-if [[ $1 != "S" && $1 != "A" && $1 != "B" ]]; then
-	echo "Usage: $0 {S | A | B}"
-	exit 1
-fi
+[[ $# -ne 1 ]] && echo "${HELP_STR}" && exit 1
+
+[[ $1 != "S" && $1 != "A" && $1 != "B" && $1 != "C" ]] &&
+  echo "${HELP_STR}" && exit 2
+
+CLASS=$1
 
 for W in bt cg dc ep ft is lu mg sp ua; do
-	cd $W
-	rm -f npbparams.h
-	ln -s npbparams-$1.h npbparams.h
-	cd ..
+  cd $W
+  rm -f ${HEADER_FILE}
+
+  BMK_CLASS=$CLASS
+  if [[ $W == "dc" && $CLASS == "C" ]]; then
+    echo "warning: using class ${BMK_CLASS} for benchmark ${W}"
+    BMK_CLASS="B"
+  fi
+
+  ln -s npbparams-${BMK_CLASS}.h ${HEADER_FILE}
+  cd ..
 done
