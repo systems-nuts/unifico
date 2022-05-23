47543b0ac9e0e27e06f82fc34d936bb7496d5cc4
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-05-23 12:25:28 +0100

    Added test cases related to cg::sprnvc.
    Fixes: #93, #102, #104

---
+++ layout/cg-sprnvc-1/main.c
@@ -0,0 +1,25 @@
+double amult;
+
+void randlc(double a)
+{
+    a++;
+}
+
+void randlc_ptr(double* a)
+{
+    a = 5;
+}
+
+static void sprnvc()
+{
+  amult = 12345.0;
+  while (1) {
+    randlc(amult);
+  }
+}
+
+int main()
+{
+   sprnvc();
+   return 0;
+}
---
+++ layout/cg-sprnvc-2/main.c
@@ -0,0 +1,20 @@
+double amult;
+
+void randlc(double a)
+{
+    a++;
+}
+
+static void sprnvc()
+{
+  while (1) {
+    randlc(amult);
+    randlc(amult);
+  }
+}
+
+int main()
+{
+   sprnvc();
+   return 0;
+}
