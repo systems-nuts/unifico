48c7b63c63623ca386efcf0db424b6e03a53a57f
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-05-31 17:55:36 +0100

    Added test cases related to cg::main, regarding the constant cost model.

---
+++ layout/cg-main-2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define NA      75000
+#define NONZER  13
+#define NZ    (NA*(NONZER+1)*(NONZER+1))
+#define NAZ   (NA*(NONZER+1))
+
+static int naa;
+static int nzz;
+
+void simple(int n)
+{
+    return;
+}
+
+int main()
+{
+    int x, y;
+    x = NA - 1;
+    printf("\n");
+    y = NA;
+
+    printf(" Size: %11d\n", NA);
+
+    naa = NA;
+    nzz = NZ;
+
+    printf("\n");
+
+    return 0;
+}
