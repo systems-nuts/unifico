c06fd6563aed44a7e5a99d093cf6f6667288569a
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-06-01 11:06:53 +0100

    Added config common Makefiles for docker images

---
+++ layout/common/docker.defs.mk
@@ -0,0 +1,22 @@
+POPCORN ?= /usr/local/popcorn
+
+PREFIX := /tools
+
+LLVM_TOOLCHAIN ?= ${PREFIX}/toolchain-unasl/bin
+
+PROJECT_DIR ?= ../..
+
+# Lib musl directories per architecture
+MUSL_TOOLCHAIN ?= ${PREFIX}/musl/
+ARM64_MUSL	?= $(MUSL_TOOLCHAIN)/aarch64
+X86_64_MUSL	?= $(MUSL_TOOLCHAIN)/x86_64
+
+# Directory of libgcc & libgcc_eh for aarch64 compiler
+ARM64_LIBGCC   ?= $(shell dirname \
+									$(shell aarch64-linux-gnu-gcc -print-libgcc-file-name))
+
+# For llvm-mca tool TODO
+MCA	?= $(LLVM_TOOLCHAIN)/llvm-mca
+ARM64_CPU	?= thunderx2t99
+X86_64_CPU	?= btver2
+MCA_RESULT_DIR	?= ../mca-results/reg-pressure-O0
