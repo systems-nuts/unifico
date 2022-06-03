6d9eb123b5299363ac206042debbfd1ce0d4e28d
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-03 19:50:54 +0100

    Added test cases related to cg::main, regarding the register offset addressing.
    Closes https://github.com/systems-nuts/UnASL/issues/117

---
+++ layout/cg-main-3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void simple(int n)
+{
+    return;
+}
+
+int main()
+{
+    int t_names[4];
+    double t;
+
+    for (int i = 0; i < 10; i++) {
+        printf("  %8s:\n", t_names[i]);
+    }
+
+    return 0;
+}
