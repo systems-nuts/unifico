9aef0c6fd814acd8aa00d29a4524a3513fdba902
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-07 12:21:49 +0100

    Added test cases related to cg::main, regarding the different call site order.

---
+++ layout/cg-main-4/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int t_names[4];
+    double t;
+
+    for (int i = 0; i < 10; i++) {
+      if (i == 5) {
+        printf("  %8s:\n", t_names[i]);
+      } else {
+        printf("  %8s:%9.3f  (%6.2f%%)\n", t_names[i], t, 100.0);
+       }
+    }
+
+    return 0;
+}
