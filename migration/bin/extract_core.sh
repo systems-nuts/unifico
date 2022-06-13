77820aa4909f629bf78e104e9e155ee25811ef3d
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-06-13 18:10:59 +0100

    Rearrange file structure

--- migration/bin/extract_core.sh
+++ migration/bin/extract_core.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
 
-gdb -x ${SCRIPT_DIR}/../gdb_util/migrate.py -ex "migrate ${1}" -ex "run" -ex "quit" ${2}
+gdb -x ${SCRIPT_DIR}/../lib/util/gdb/migrate.py -ex "migrate ${1}" -ex "run" -ex "quit" ${2}
