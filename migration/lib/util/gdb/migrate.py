db6fac45e8702672d7f4a37d5f056909d1b14703
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-06-14 12:27:56 +0100

    Add gdb custom command

--- migration/lib/util/gdb/migrate.py
+++ migration/lib/util/gdb/migrate.py
@@ -2,6 +2,9 @@
 
 import gdb
 import json
+import time
+
+tstamp = None
 
 
 class CoredumpBreakpoint(gdb.Breakpoint):
@@ -26,4 +29,18 @@ class _Migrate(gdb.Command):
         CoredumpBreakpoint(f"{self.filename}:{self.line}", gdb.BP_BREAKPOINT)
 
 
+class _TimedRun(gdb.Command):
+    def __init__(self) -> None:
+        gdb.Command.__init__(
+            self, "timed-run", gdb.COMMAND_RUNNING, gdb.COMPLETE_FILENAME
+        )
+
+    def invoke(self, arg, from_tty) -> None:
+        global tstamp
+        tstamp = time.time()
+
+        gdb.execute("run")
+
+
 _Migrate()
+_TimedRun()
