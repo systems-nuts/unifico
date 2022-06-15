0a54a1fa1e44482cbf7b5cb19f27fbe3228a489b
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-15 12:50:39 +0100

    Added test cases related to ep::main, regarding indexing of arrays.

---
+++ layout/ep-main-1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double randlc(double *a, double b )
+{
+    return 0.0;
+}
+
+int main()
+{
+  double dum[3] = {1.0, 1.0, 1.0};
+
+  dum[0] = randlc(&dum[1], dum[2]);
+  dum[0] = randlc(&dum[1], dum[2]);
+
+  return 0;
+}
