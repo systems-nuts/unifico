b3ad1538364d3f892dd2900931837fcace94eb50
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-22 12:42:28 +0100

    Added more test cases for previous seg fault errors.

---
+++ layout/ep-main-3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define N        16
+
+double x[10];
+
+int main()
+{
+    double n, m;
+
+    for (int i = 0; i < N; i++) {
+      n = 2.0 * x[2*i] - 1.0;
+      m = 2.0 * x[2*i+1] - 1.0;
+      n = n * n + m * m;
+    }
+    printf("%lf\n", x[0]);
+
+    return 0;
+}
