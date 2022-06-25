3bf0788ce50e9ecc277becdef805c937fbc2bbf9
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-25 21:34:12 +0100

    Added test cases related to accessing 2d arrays of structs.
    Closes #141

---
+++ layout/ft-fft3d-2/main.c
@@ -0,0 +1,26 @@
+#include "type.h"
+
+void simple()
+{
+    return;
+}
+
+
+static void Swarztrauber(int xd1, void *ox)
+{
+  dcomplex (*x)[xd1] = (dcomplex (*)[xd1])ox;
+
+  int i = 5, k = 2, m = 3;
+  dcomplex x11;
+
+  simple();
+
+  x11 = x[i][k];
+}
+
+
+int main()
+{
+    void *p;
+    Swarztrauber(1, p);
+}
