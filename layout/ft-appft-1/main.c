c482c21273843a22fbf40b14e49201ad49a57248
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-06-26 21:08:01 +0100

    More testcases.
    Closes #151, #152

---
+++ layout/ft-appft-1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "global.h"
+
+
+static double twiddle[NZ][NY][NX+1];
+
+
+double simple(double n)
+{
+    return n;
+}
+
+
+int main()
+{
+  int i, j, k;
+
+  for (i = 0; i < 10; i++) {
+    for (k = 0; k < 10; k++) {
+      for (j = 0; j < 10; j++) {
+        twiddle[i][k][j] = simple((double)(j));
+      }
+    }
+  }
+}
---
+++ layout/ft-appft-2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define NX             512
+#define NY             256
+
+static double twiddle[NX][NY+1];
+
+
+double simple(double n)
+{
+    return n;
+}
+
+
+int main()
+{
+  int i;
+
+  while (1)
+      twiddle[i][i] = simple((double)(i));
+}
