e0d82bf0847dabd8e48139a1ceb732b6630c929f
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-07-28 08:53:43 +0100

    Add makefile defs

---
+++ layout/common/sterope-popcorn.defs.mk
@@ -0,0 +1,20 @@
+POPCORN ?= /usr/local/popcorn
+
+LLVM_TOOLCHAIN ?= $(POPCORN)/bin
+
+PROJECT_DIR ?= ../..
+
+# Lib musl directories per architecture
+MUSL_TOOLCHAIN ?= /bulk/wb/unasl-project/musl-toolchains/llvm-9
+ARM64_MUSL	?= $(MUSL_TOOLCHAIN)/aarch64
+X86_64_MUSL	?= $(MUSL_TOOLCHAIN)/x86_64
+
+# Directory of libgcc & libgcc_eh for aarch64 compiler
+ARM64_LIBGCC   ?= $(shell dirname \
+									$(shell aarch64-linux-gnu-gcc -print-libgcc-file-name))
+
+# For llvm-mca tool TODO
+MCA	?= $(LLVM_TOOLCHAIN)/llvm-mca
+ARM64_CPU	?= thunderx2t99
+X86_64_CPU	?= btver2
+MCA_RESULT_DIR	?= ../mca-results/reg-pressure-O0
