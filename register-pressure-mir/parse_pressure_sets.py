3371834d5dabc48c66640f570ac0b61c4b38fe3c
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-08-02 18:01:47 +0100

    First draft of parsing register pressure sets.
    Given the debug output of the machine scheduler, the name of the
    basic block to check, and a substring of the LLVM MI, we return
    a dictionary of pressure sets (names -> pressure).

--- register-pressure-mir/parse_pressure_sets.py
+++ register-pressure-mir/parse_pressure_sets.py
@@ -3,7 +3,10 @@ import re
 
 # The debug output of `machine-scheduler` is expected as an input.
 # The following lines are detected and parsed:
-
+#
+# main:%bb.2 for.body
+#   From: %18:gr64_nosp = MOV64rm %stack.3.i, 1, $noreg, 0, $noreg :: (dereferenceable load 8 from %ir.i)
+# ...
 # Max Pressure: GR32_BPSP=1
 # GR32_SIDI=1
 # GR32_DIBP+GR32_SIDI=1
@@ -20,10 +23,6 @@ import re
 # TODO: How to calculate pressure, considering the class overlaps?
 
 
-# This verbose and more readable regex form, requires the re.VERBOSE flag in re.compile
-
-
-# This verbose and more readable regex form, requires the re.VERBOSE flag in re.compile
 REG_CLASS_REGEX = re.compile(
     r"""
     (Max\sPressure:\s)? # First appearing class starts with this
@@ -33,6 +32,58 @@ REG_CLASS_REGEX = re.compile(
 """,
     re.VERBOSE,
 )
+
+
+def get_pressure_sets(input_file, function_block, machine_instruction):
+    """
+    Get the pressure sets at a specific LLVM Machine Instruction,
+    from the particular function block, e.g.,
+
+    `%18:gr64_nosp = MOV64rm %stack.3.i, 1, $noreg, 0, $noreg` of `main:%bb.2 for.body`
+
+    The input file is the output of the `machine-scheduler` pass.
+
+    @param input_file: Path to debug output file
+    @param function_block: E.g., loop:%bb.0
+    @param machine_instruction: LLVM MI, e.g., `MOV32mi %stack.0.retval, 1, $noreg, 0`
+    @return: Dictionary with pressure sets
+    """
+    with open(input_file, "r") as objdump_file:
+        lines = objdump_file.readlines()
+
+        start_counting = False
+        skip_function = True
+        skip_instruction = True
+        pressure_sets = {}
+
+        for index, line in enumerate(lines):
+
+            if line.startswith(function_block):
+                skip_function = False
+
+            if machine_instruction in line:
+                skip_instruction = False
+
+            if line.startswith("Max Pressure:"):
+                start_counting = True
+
+            match_result = REG_CLASS_REGEX.match(line)
+            if (
+                match_result
+                and not skip_function
+                and not skip_instruction
+                and start_counting
+            ):
+                pressure_sets[match_result.group(2)] = match_result.group(3)
+                continue
+
+            if line.startswith("Live In:"):
+                start_counting = False
+                skip_function = True
+                skip_instruction = True
+
+    return pressure_sets
+
 
 arg_parser = argparse.ArgumentParser(
     description="Register pressure calculator at the MIR level."
@@ -48,25 +99,12 @@ arg_parser.add_argument(
 
 
 def __main__(args: argparse.Namespace):
-    with open(args.input_file, "r") as objdump_file:
-
-        lines = objdump_file.readlines()
-
-        start_counting = False
-
-        for index, line in enumerate(lines):
-
-            if line.startswith("Max Pressure:"):
-                start_counting = True
-
-            match_result = REG_CLASS_REGEX.match(line)
-            if match_result and start_counting:  # Inside a function's code
-                print(match_result.group(0))
-                continue
-
-            if line.startswith("Live In:"):
-                start_counting = False
-                print("-------")
+    pressure_sets = get_pressure_sets(
+        args.input_file,
+        "main:%bb.2 for.body",
+        "%18:gr64_nosp = MOV64rm %stack.3.i, 1, $noreg, 0, $noreg",
+    )
+    print(pressure_sets)
 
 
 if __name__ == "__main__":
