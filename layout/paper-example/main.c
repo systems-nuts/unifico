03e0d950af3313128c694706f7d5772076f5ccf8
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-10-27 18:49:23 +0100

    Added example for paper motivation.

---
+++ layout/paper-example/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+double fmul(double* x) {
+    static double r = 0.5;
+    return (*x) * r;
+}
+
+
+int main()
+{
+    double x = 1.0;
+    double sum = 0;
+    for (int i = 0; i < 16; i++) {
+        sum = fmul(&x);
+    }
+    printf("%f", sum);
+    return 0;
+}
