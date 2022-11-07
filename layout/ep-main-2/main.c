ec5c6adfca7d304456caa6260fdc8caef07e59af
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-11-07 16:03:56 +0000

    Added ep-main-2 microbenchmark.

---
+++ layout/ep-main-2/main.c
@@ -0,0 +1,20 @@
+typedef enum { false, true } logical;
+
+void simple(int n)
+{
+    return;
+}
+
+int main()
+{
+  logical verified;
+
+  simple(0);
+  verified = true;
+
+  if (verified) {
+    verified = false;
+  }
+
+  return 0;
+}
