b8d0b3fa78b65772b4810659415aa8a97b243b36
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-12-20 15:57:16 +0000

    Added initial implementation of ascii-art stack printing for arm.

---
+++ stack-ascii-printer/tests/x29_x30_w0_prev_frame.asm
@@ -0,0 +1,14 @@
+# RUN: print_ascii_stack.py -i %s -f main -c 1 | filecheck %s
+
+# CHECK-NEXT: 0x0  : ||x29-----|x30-----|| : 0x0
+# CHECK-NEXT: 0x10 : ||w0------|--------|| : 0x10
+# CHECK-NEXT:
+# CHECK-NEXT: sp : 0x0
+# CHECK-NEXT: x29: 0x0
+
+0000000000501040 main:
+; {
+  501040: ff 43 03 d1                  	sub	sp, sp, #0x10
+  50104c: fd 03 03 91                  	add	x29, sp, #0x0
+  501044: f3 5f 00 f9                  	str	w0, [sp, #0x10]
+  501048: fd 7b 0c a9                  	stp	x29, x30, [sp]
