c631925c1d877f5b8f18a2e575c92085c1af1ed1
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-02-01 21:11:46 +0000

    Added separate pin tool for remote heap accesses

---
+++ exploration/pin-tools/heap-accesses/run_array_write.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#cd ~/Documents/phd/unified_abi/layout/call-leaf
+#make clean && make init
+/opt/pin-3.20-98437-gf02b61307-gcc-linux/pin -t obj-intel64/heap_accesses.so -- /home/blackgeorge/Documents/phd/unified_abi/layout/array-write/a.out
