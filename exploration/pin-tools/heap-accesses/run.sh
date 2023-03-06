58c91a534e35ac151d431db421e53baea3601cbe
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-03-06 18:29:22 +0000

    Removed old pin-tools directory

--- exploration/pin-tools/heap-accesses/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#cd ~/Documents/phd/unified_abi/layout/call-leaf || exit
-#make clean && make init
-/opt/pin-3.20-98437-gf02b61307-gcc-linux/pin -t obj-intel64/heap_accesses.so -- /home/blackgeorge/CLionProjects/fait-maison-spmv/src/sequential/c/run_float /home/blackgeorge/CLionProjects/fait-maison-spmv/src/sequential/c/inputs/huge.mtx csr
