7024b56a3a596fdd8949135466cba17b93d1f055
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-06-25 16:58:34 +0100

    [NPB runner] Renamed variable to show that is talks about the whole NPB suite

--- npb/runs/npb_run.py
+++ npb/runs/npb_run.py
@@ -197,15 +197,15 @@ class NPBRunner:
         if not self.args.dryrun and not os.path.exists(self.bin_dir):
             os.mkdir(self.bin_dir)
 
-        build_dir = os.getenv("NPB_PATH")
+        suite_dir = os.getenv("NPB_PATH")
 
-        if not os.path.exists(build_dir):
-            self.eprint(f'Error: "{build_dir}" does not exist!')
+        if not os.path.exists(suite_dir):
+            self.eprint(f'Error: "{suite_dir}" does not exist!')
             exit(2)
 
-        print(f"Changing working dir: {build_dir}")
+        print(f"Changing working dir: {suite_dir}")
         if not self.args.dryrun:
-            os.chdir(build_dir)
+            os.chdir(suite_dir)
 
         for benchmark in self.cfg["executables"]:
             bench_cfg = self.cfg["*"].copy()
