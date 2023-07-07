6f933f7916ebca365fa6e0d83c388fec2bda5c27
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-07-07 21:07:52 +0100

    [layout] Add FUNC cmd option to `before_after_<FILE>` target
    By running, `make before_after_<FILE> PASS=<PASS> FUNC=<FUNC> we can get
    the MIR before and after only the specific function, for easier debugging.

--- layout/common/common.mk
+++ layout/common/common.mk
@@ -423,10 +423,10 @@ pass_structure_%: %_cs_align.json %_opt.ll %_aarch64.o
 	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_ARM64) -march=aarch64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -debug-pass=Structure 2>$(ARM64_BUILD)/$*_pass_structure.txt
 
 before_after_pass_%: %_cs_align.json %_opt.ll %_aarch64.o
-	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_X86) -march=x86-64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-before=$(PASS) 2>$(X86_64_BUILD)/$*_before_$(PASS).txt
-	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_X86) -march=x86-64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-after=$(PASS) 2>$(X86_64_BUILD)/$*_after_$(PASS).txt
-	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_ARM64) -march=aarch64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-before=$(PASS) 2>$(ARM64_BUILD)/$*_before_$(PASS).txt
-	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_ARM64) -march=aarch64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-after=$(PASS) 2>$(ARM64_BUILD)/$*_after_$(PASS).txt
+	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_X86) -march=x86-64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-before=$(PASS) --filter-print-funcs=$(FUNC) 2>$(X86_64_BUILD)/$*_before_$(PASS).txt
+	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_X86) -march=x86-64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-after=$(PASS) --filter-print-funcs=$(FUNC) 2>$(X86_64_BUILD)/$*_after_$(PASS).txt
+	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_ARM64) -march=aarch64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-before=$(PASS) --filter-print-funcs=$(FUNC) 2>$(ARM64_BUILD)/$*_before_$(PASS).txt
+	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_ARM64) -march=aarch64 -filetype=obj -callsite-padding=$< -o temp.o $(word 2,$^) -print-after=$(PASS) --filter-print-funcs=$(FUNC) 2>$(ARM64_BUILD)/$*_after_$(PASS).txt
 
 init_before_after_pass_%: %_opt.ll
 	$(QUIET) $(LLC) $(LLC_FLAGS) $(LLC_FLAGS_X86) -march=x86-64 -filetype=obj -o temp.o $< -print-before=$(PASS) 2>$(X86_64_BUILD)/$*_before_$(PASS).txt
