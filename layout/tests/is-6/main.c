301eaf330003c485af86cd59fcbd3ddd5b2d6679
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-08-02 15:43:14 +0100

    [layout] Restructure is-6 test to cover -O1

--- layout/tests/is-6/main.c
+++ layout/tests/is-6/main.c
@@ -6,12 +6,13 @@ int main(int argc, char **argv)
 {
     int timer_on;
     double timecounter = 3.0;
-    double t_percent = 4.0;
+    volatile double t_percent = 4.0;
 
     timecounter = timer_read(1);
     t_percent = timecounter * 101.0;
     timecounter = timer_read(2);
-    t_percent = timecounter * 101.0;
+    t_percent *= timecounter * 101.0;
+    printf("t_percent = %f\n", t_percent);
 
     return 0;
 }
