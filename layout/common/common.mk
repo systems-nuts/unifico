fb8ac28726f565f1edd84a1b4625a82949b8dcfb
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-08-04 13:25:22 +0100

    [layout] Add flag to avoid converting select to fsetcc
    Addresses: https://github.com/systems-nuts/unifico/issues/284

--- layout/common/common.mk
+++ layout/common/common.mk
@@ -76,7 +76,7 @@ override LLC_FLAGS_X86   += -mattr=+aarch64-constant-cost-model -x86-enable-simp
 
 # Other optimizations
 override LLC_FLAGS_ARM64 += -mattr=-avoid-f128
-override LLC_FLAGS_X86   += -mattr=+force-vector-mem-op,+avoid-opt-mul-1 -no-x86-call-frame-opt -disable-x86-frame-obj-order -enable-lea32 -mul-constant-optimization=false
+override LLC_FLAGS_X86   += -mattr=+force-vector-mem-op,+avoid-opt-mul-1,+avoid-select-to-fsetcc -no-x86-call-frame-opt -disable-x86-frame-obj-order -enable-lea32 -mul-constant-optimization=false
 
 endif # EXPERIMENT_MODE
 endif # UNMODIFIED_LLVM
