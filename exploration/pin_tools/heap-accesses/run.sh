052ec4b917a23b12e171ce0359595a245bb9fff5
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-12-01 12:36:48 +0000

    [Intel Pin Runner] Update run script

--- exploration/pin_tools/heap-accesses/run.sh
+++ exploration/pin_tools/heap-accesses/run.sh
@@ -2,4 +2,4 @@
 
 #cd ~/Documents/phd/unified_abi/layout/call-leaf || exit
 #make clean && make init
-/opt/pin-3.20-98437-gf02b61307-gcc-linux/pin -t obj-intel64/heap_accesses.so -o temp.txt -- /home/blackgeorge/CLionProjects/DAMOV/workloads/ligra/apps/Triangle /home/blackgeorge/CLionProjects/DAMOV/workloads/ligra/inputs/rMatGraph_J_5_100
+/opt/pin-3.20-98437-gf02b61307-gcc-linux/pin -t obj-intel64/heap_accesses.so -f edgeMapDense -o temp.txt -- /home/blackgeorge/CLionProjects/DAMOV/workloads/ligra/apps/Triangle /home/blackgeorge/CLionProjects/DAMOV/workloads/ligra/inputs/rMat_100K
